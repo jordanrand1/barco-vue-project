5d5033535cb2c1fed5e39210961f32fbfef076e4
jorandrand1 <jorrand@gmail.com>
2020-01-20 22:44:44 -0700

    lint fixes

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,11 @@
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-import Home from "./components/Home";
-import Profile from "./components/Profile";
 import Nav from "./components/Nav";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
-    Home,
-    Profile,
     Nav
   }
 };
